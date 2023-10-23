df00a10ac0057b367ae4651736a991a7cff551b9
OlesiaZaozirna <137696365+OlesiaZaozirna@users.noreply.github.com>
2023-10-23 22:00:10 +0300

    Done task10

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,56 @@
+/*Напиши скрипт створення і очищення колекції елементів. Користувач вводить кількість елементів в input і натискає кнопку Створити, після чого рендериться колекція. Натисненням на кнопку Очистити, колекція елементів очищається.
+
+<div id="controls">
+  <input type="number" min="1" max="100" step="1" />
+  <button type="button" data-create>Create</button>
+  <button type="button" data-destroy>Destroy</button>
+</div>
+
+<div id="boxes"></div>
+
+Створи функцію createBoxes(amount), яка приймає один параметр - число. Функція створює стільки <div>, скільки вказано в amount і додає їх у div#boxes.
+
+Розміри найпершого <div> - 30px на 30px.
+Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
+Всі елементи повинні мати випадковий колір фону у форматі HEX. Використовуй готову функцію getRandomHexColor для отримання кольору.
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
+
+*/
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+const input = document.querySelector("input");
+const boxesContainer = document.querySelector("#boxes");
+
+let div = [];
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i += 1) {
+    div[i] = document.createElement("div");
+    div[i].style.width = `${30 + [i] * 10}px`;
+    div[i].style.height = `${30 + [i] * 10}px`;
+    div[i].style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(div[i]);
+  }
+}
+function destroyBoxes() {
+  while (boxesContainer.firstChild) {
+    boxesContainer.removeChild(boxesContainer.firstChild);
+  }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+btnCreate.addEventListener("click", () => {
+  createBoxes(input.value);
+});
+btnDestroy.addEventListener("click", () => {
+  destroyBoxes();
+});
